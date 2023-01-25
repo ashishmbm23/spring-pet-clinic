71b8af01179ee92c499134ac705fee54d2d5edf5
Ashish Verma <32788353+ashishmbm23@users.noreply.github.com>
2023-01-25 14:00:50 +0530

    Refactored services to base service. closes #11

--- pet-clinic-data/src/main/java/com/ashish/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/ashish/services/OwnerService.java
@@ -2,15 +2,7 @@ package com.ashish.services;
 
 import com.ashish.model.Owner;
 
-import java.util.Set;
-
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner, Long>{
 
     Owner findByLastName(String name);
-
-    Owner findById(Long id);
-
-    Owner save(Owner owner);
-
-    Set<Owner> findAll();
 }
