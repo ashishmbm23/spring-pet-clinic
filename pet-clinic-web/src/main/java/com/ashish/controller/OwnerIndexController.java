9924cb849382fabff648704b3038b9effa4349d6
Ashish Verma <32788353+ashishmbm23@users.noreply.github.com>
2023-01-26 13:42:30 +0530

    added vet index page and controller. closes #7

--- pet-clinic-web/src/main/java/com/ashish/controller/OwnerIndexController.java
+++ pet-clinic-web/src/main/java/com/ashish/controller/OwnerIndexController.java
@@ -3,10 +3,11 @@ package com.ashish.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/owners")
 @Controller
 public class OwnerIndexController {
 
-    @RequestMapping({"/owners", "/owners/index", "/owners/index.html"})
+    @RequestMapping({"","/", "/index", "/index.html"})
     public String getListOfOwners(){
         return "owners/index";
     }
--- pet-clinic-web/src/main/java/com/ashish/controller/VetsIndexController.java
+++ pet-clinic-web/src/main/java/com/ashish/controller/VetsIndexController.java
@@ -3,10 +3,11 @@ package com.ashish.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/vets")
 @Controller
 public class VetsIndexController {
 
-    @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
+    @RequestMapping({"","/", "/index", "/index.html"})
     public String getListOfVets(){
         return "vets/index";
     }
