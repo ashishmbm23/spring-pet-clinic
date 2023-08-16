8ff68a233621e809b2649e210ff0899e633a002c
Ashish <ashishmbm23@gmail.com>
2023-08-16 15:05:28 +0530

    Implemented PetType JPA Service. closes #42

---
+++ pet-clinic-data/src/main/java/com/ashish/springdatajpa/PetTypeSDJPAService.java
@@ -0,0 +1,47 @@
+package com.ashish.springdatajpa;
+
+import com.ashish.model.PetType;
+import com.ashish.repository.PetTypeRepository;
+import com.ashish.services.PetTypeService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("spring.service.jpa")
+public class PetTypeSDJPAService implements PetTypeService {
+    private final PetTypeRepository petTypeRepository ;
+
+    public PetTypeSDJPAService(PetTypeRepository petTypeRepository) {
+        this.petTypeRepository = petTypeRepository;
+    }
+
+    @Override
+    public Set<PetType> findAll() {
+        Set<PetType> petTypes = new HashSet<>();
+        petTypeRepository.findAll().forEach(petTypes::add);
+        return petTypes;
+    }
+
+    @Override
+    public PetType findById(Long aLong) {
+        return petTypeRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        petTypeRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        petTypeRepository.deleteById(aLong);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return petTypeRepository.save(object);
+    }
+}
