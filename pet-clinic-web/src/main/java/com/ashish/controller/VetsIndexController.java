df732654c560d446fcd46d2427c020c99f26d8b9
Ashish <ashishmbm23@gmail.com>
2023-10-31 22:17:35 +0530

    Implemented list of vets json end point and little modification.

--- pet-clinic-web/src/main/java/com/ashish/controller/VetsIndexController.java
+++ pet-clinic-web/src/main/java/com/ashish/controller/VetsIndexController.java
@@ -1,9 +1,14 @@
 package com.ashish.controller;
 
+import com.ashish.model.Vet;
 import com.ashish.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 
 @Controller
@@ -20,5 +25,10 @@ public class VetsIndexController {
     {
         model.addAttribute("vets", vetService.findAll());
         return "vets/index";
+    }
+
+    @GetMapping("/api/vets")
+    public @ResponseBody Set<Vet> getListOfVets(){
+        return vetService.findAll();
     }
 }
